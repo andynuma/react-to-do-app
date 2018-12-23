da2c27c197da0b4a03fbd7e67f585bd406026a3a
Ryota Ishikawa <c126001a@comwork.c.oka-pu.ac.jp>
2018-12-24 07:57:32 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { TodoApp } from "./TodoApp";
+
+render(<TodoApp />, document.getElementById("root"));
